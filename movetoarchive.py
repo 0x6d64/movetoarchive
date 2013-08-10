2b285dd2a3a1b4f19e3a983bc17a5f8fdf9372a4
0x6d64 <0x6d64@martin-demling.de>
2013-08-10 22:06:28 +0200

    init-commit

---
+++ movetoarchive.py
@@ -0,0 +1,11 @@
+#!/opt/local/bin/python
+# movetoarchive - quickly archive files in a year/month folder structure
+# (c) 2013 by Martin Demling
+# License: GPL3
+# https://github.com/0x6d64/ fixme: add github URL
+
+import os 
+import shutil
+
+print "aktuelles Verzeichnis: "+ os.getcwd()
+
