a9c44968b7dc11c42f8993da5c062162d48c22ee
0x6d64 <0x6d64@martin-demling.de>
2013-08-11 18:12:04 +0200

    it works

--- movetoarchive.py
+++ movetoarchive.py
@@ -6,6 +6,47 @@
 
 import os 
 import shutil
+import sys
+import time
+#---------------------------------    
+def PrintSeparator():
+    if sys.stdout.isatty():
+        rows, columns = os.popen('stty size', 'r').read().split()
+        TerminalRows = int(rows)
+        TerminalColumns = int(columns)
+    else:
+        TerminalRows = 0
+        TerminalColumns = 30
+    print(TerminalColumns * "-")
+#---------------------------------
+# PrintSeparator()
+# print "debug-info:"
+# print "in dir: " + os.getcwd()
+# 
+# for index, arg in enumerate(sys.argv):
+#     print str(index) + ". arg: " + str(arg)
+# PrintSeparator()
 
-print "aktuelles Verzeichnis: "+ os.getcwd()
+CurrentDir = os.getcwd()
+FileCount = 0
+CreateCount = 0
 
+for file in os.listdir(CurrentDir):
+    if os.path.isfile(file):
+        TimeOfLastChange = time.localtime(os.path.getmtime(file))
+        YearString = str(TimeOfLastChange.tm_year)
+        MonthString = str(TimeOfLastChange.tm_mon)
+        if len(MonthString) == 1:
+            MonthString = "0" + MonthString  # pad one digit numbers with zero
+        YearDirAbspath = CurrentDir + "/" + YearString
+        MonthDirAbspath = YearDirAbspath + "/" + YearString + "-" + MonthString  # dir in format "2013-9"
+        if not os.path.isdir(YearDirAbspath):
+            os.mkdir(YearDirAbspath, 0755)
+            CreateCount += 1
+        if not os.path.isdir(MonthDirAbspath):
+            os.mkdir(MonthDirAbspath, 0755)
+            CreateCount += 1
+        shutil.move(file, MonthDirAbspath)
+        FileCount += 1
+
+print "%d files moved and %d directories created" % (FileCount, CreateCount)
