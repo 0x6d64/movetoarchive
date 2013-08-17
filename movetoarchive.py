0273a91a2df4530ef6a644def030a695544daaa6
0x6d64 <0x6d64@martin-demling.de>
2013-08-17 22:41:51 +0200

    -arguments for year folder only, quiet, verbose

--- movetoarchive.py
+++ movetoarchive.py
@@ -6,10 +6,12 @@
 # License: GPL3
 # https://github.com/0x6d64/ fixme: add github URL
 
-import os 
-import shutil
 import sys
+import os 
 import time
+import shutil
+import argparse
+
 #---------------------------------    
 def PrintSeparator():
     if sys.stdout.isatty():
@@ -29,13 +31,25 @@ def PrintSeparator():
 #     print str(index) + ". arg: " + str(arg)
 # PrintSeparator()
 #---------------------------------
+parser = argparse.ArgumentParser()
+group1 = parser.add_mutually_exclusive_group()
+group1.add_argument("-m", "--month", help="create subfolders for years and months (default)", action="store_true")
+group1.add_argument("-y", "--year", help="create subfolders for years", action="store_true")
+group2 = parser.add_mutually_exclusive_group()
+group2.add_argument("-v", "--verbose", help="verbose output (default)", action="store_true")
+group2.add_argument("-q", "--quiet", help="quiet output", action="store_true")
+args= parser.parse_args()
+    
 FolderMode = 0755
 CurrentDir = os.getcwd() #get the directory where the script is run from
 FileCount = 0 #only for stats
 CreateCount = 0
 
+
 for FilePath in os.listdir(CurrentDir): #handle all files in current dir
     if os.path.isfile(FilePath): #deal only with files, not folders
+        if args.verbose:
+            print("handling file %s") % FilePath
         TimeOfLastChange = time.localtime(os.path.getmtime(FilePath))
         YearString = str(TimeOfLastChange.tm_year)
         MonthString = str(TimeOfLastChange.tm_mon)
@@ -47,17 +61,26 @@ for FilePath in os.listdir(CurrentDir): #handle all files in current dir
         MonthDirAbspath = YearDirAbspath + "/" + YearString + "-" + MonthString  # dir in format "2013-9"
         
         if not os.path.isdir(YearDirAbspath):
+            if args.verbose:
+                print("creating folder ./%s") % YearString
             os.mkdir(YearDirAbspath, FolderMode)
             CreateCount += 1
-        if not os.path.isdir(MonthDirAbspath):
-            os.mkdir(MonthDirAbspath, FolderMode)
-            CreateCount += 1
-        
-        NewFileAbspath = MonthDirAbspath + "/" + os.path.basename(FilePath)
+        if not args.year:
+            if not os.path.isdir(MonthDirAbspath):
+                if args.verbose:
+                    print("creating folder ./%s/%s") % (YearString, MonthString)
+                os.mkdir(MonthDirAbspath, FolderMode)
+                CreateCount += 1
         
         TempAttributes = os.stat(FilePath) #save file attributes in temp var
-        shutil.move(FilePath, MonthDirAbspath)
+        if args.month:
+            NewDirAbsPath = MonthDirAbspath
+        else:
+            NewDirAbsPath = YearDirAbspath
+        shutil.move(FilePath, NewDirAbsPath)
+        NewFileAbspath = NewDirAbsPath + "/" + os.path.basename(FilePath)
         os.utime(NewFileAbspath, (TempAttributes.st_atime, TempAttributes.st_mtime)) #restore file attributes
         FileCount += 1
-
-print "%d files moved and %d directories created" % (FileCount, CreateCount)
+        
+if not args.quiet:
+    print "%d files moved and %d directories created" % (FileCount, CreateCount)
--- todo.md
+++ todo.md
@@ -1,8 +1,12 @@
 # todo
+
+
+
+# later
+- think about mechanism to restructure a folder (pull files from subfolders and arrange them into archive structure)
+
+#done
 - argument parser
 - print help text
 - handle verbose argument
 - handle argument for year folders only
-
-# later
-- think about mechanism to restructure a folder (pull files from subfolders and arrange them into archive structure)
